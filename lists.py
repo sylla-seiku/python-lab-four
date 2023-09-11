5d8d618405822c075acd5662403efa1c6b24471b
sylla-seiku <seikusylla@gmail.com>
2023-09-11 11:39:31 -0400

    Learning 2D lists

--- lists.py
+++ lists.py
@@ -1,3 +1,25 @@
+#learning 2D lists
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+matrix[0][1] = [20]
+print(matrix[0][1])
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+for row in matrix:
+    for item in row:
+        print(item)
+
+
+
+'''
 #learning lists
 name = ['Seiku', 'Beyage', 'Ebrahim', 'Ahamadou', 'Kadijatou']
 print(name)
@@ -17,3 +39,4 @@ for number in numbers:
         max = number
 
 print(max)
+'''

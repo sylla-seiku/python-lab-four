82320ebf3754f273073cbe7f1630842c7fac73a1
sylla-seiku <seikusylla@gmail.com>
2023-09-11 12:14:46 -0400

    Learning List Methods.

--- lists.py
+++ lists.py
@@ -1,3 +1,63 @@
+#Learning list methods
+numbers = [5, 2, 1, 7, 4]
+numbers.append(13) # adds item to the end of list
+print(numbers)
+
+numbers = [5, 2, 1, 7, 4]
+numbers.insert(0, 10) #inserts item to the position you want in a list.
+print(numbers)
+
+numbers = [5, 2, 1, 7, 4]
+numbers.remove(7) #removes the item selected in a list
+print(numbers)
+
+numbers = [5, 2, 1, 7, 4]
+numbers.clear() #clears the list
+print(numbers)
+
+numbers = [5, 2, 1, 7, 4]
+numbers.pop() #removes last item in list.
+print(numbers)
+
+numbers = [5, 2, 1, 7, 4]
+print(numbers.index(2))# checks position of item in a list.
+
+numbers = [5, 2, 1, 7, 4]
+print(7 in numbers) # this checks position of item in a list and gives us a boolean value.
+
+numbers = [5, 2, 1, 5, 7, 4]
+print(numbers.count(5)) # this counts occurrences of item in list.
+
+numbers = [5, 2, 1, 5, 7, 4]
+print(numbers.sort()) # this sorts the list. but when we print we wont see the list
+
+numbers = [5, 2, 1, 5, 7, 4]
+numbers.sort() #but in this example we sorted the list and then print it after. 
+print(numbers)
+
+numbers = [5, 2, 1, 5, 7, 4]
+numbers.sort()
+numbers.reverse() # this will reverse the order of the list
+print(numbers)
+
+numbers = [5, 2, 1, 5, 7, 4]
+numbers2 = numbers.copy()
+numbers.append(10)
+print(numbers2)
+
+
+#practice remove duplicates from list
+numbers = [2, 2, 4, 6, 3, 4, 6, 1]
+uniques = []
+for number in numbers:
+    if number not in uniques:
+        uniques.append(number)
+
+print(uniques)
+
+
+
+'''
 #learning 2D lists
 matrix = [
     [1, 2, 3],
@@ -19,7 +79,7 @@ for row in matrix:
 
 
 
-'''
+
 #learning lists
 name = ['Seiku', 'Beyage', 'Ebrahim', 'Ahamadou', 'Kadijatou']
 print(name)
